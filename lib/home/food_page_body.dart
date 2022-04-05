2fe648680952978453b0a2ebb9ab2cc15c08a36d
Akramul Nazim Noble <akramul47@gmail.com>
2022-04-05 14:09:59 +0600

    food_page_body

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fooddelivery/utils/colors.dart';
+import 'package:fooddelivery/widgets/big_text.dart';
+import 'package:fooddelivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -29,7 +32,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
       height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
+      margin: EdgeInsets.only(left: 10, right: 10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
           color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
@@ -39,13 +42,35 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-        height: 140,
+        height: 120,
         margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: Colors.white,
             
             
+      ),
+      child: Container(
+        padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          BigText(text: "Chinese Side"),
+          SizedBox(height: 10,),
+          Row(children: [
+            Wrap(
+              children: List.generate(5, (index) {return Icon(Icons.star,
+              color: AppColors.mainColor,size: 15,);}),
+            ),
+            SizedBox(width: 10,),
+            SmallText(text: "4.5"),
+            SizedBox(width: 10,),
+            SmallText(text: "1287"),
+            SizedBox(width: 10,),
+            SmallText(text: "comments"),
+
+          ],)
+        ]),
       ),
     ),
     ),
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -7,7 +7,7 @@ import 'package:fooddelivery/widgets/small_text.dart';
 import 'food_page_body.dart';
 
 class MainFoodPage extends StatefulWidget {
-  const MainFoodPage({ Key? key }) : super(key: key);
+  const MainFoodPage({Key? key}) : super(key: key);
 
   @override
   State<MainFoodPage> createState() => _MainFoodPageState();
@@ -22,36 +22,42 @@ class _MainFoodPageState extends State<MainFoodPage> {
           Container(
             margin: EdgeInsets.only(top: 45, bottom: 15),
             padding: EdgeInsets.only(left: 20, right: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                BigText(text: "Bangladesh", color: AppColors.mainColor,),
-                Row(
+                Column(
                   children: [
-                    SmallText(text: "Chittagong", color: Colors.black54),
-                    Icon(Icons.arrow_drop_down_rounded),
+                    BigText(
+                      text: "Bangladesh",
+                      color: AppColors.mainColor,
+                    ),
+                    Row(
+                      children: [
+                        SmallText(text: "Chittagong", color: Colors.black54),
+                        Icon(Icons.arrow_drop_down_rounded),
+                      ],
+                    )
                   ],
-                )
-                ],
-            ),
-            Center(
-              child: Container(
-                width: 45,
-                height: 45,
-                child: Icon(Icons.search,
-                color: Colors.white,),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: AppColors.mainColor,
                 ),
-              ),
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-        FoodPageBody(),],
+          ),
+          FoodPageBody(),
+        ],
       ),
     );
   }
